0a7e8cae6184a28382731f9e217d620a732773a2
Jeremy White <sail.madeline@gmail.com>
2013-06-17 11:00:07 +0800

    Converted from a Component project to a class library.

--- Mangosteen/Panels/WheelPanel.cs
+++ Mangosteen/Panels/WheelPanel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace Mangosteen.Panels
 {
-    class WheelPanel
+    class WheelPanel : Panel
     {
     }
 }
