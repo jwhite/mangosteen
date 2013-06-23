7cb7e8107e9e855c7f5d6556d29e4674fccc35f5
Jeremy White <sail.madeline@gmail.com>
2013-06-23 13:40:01 +0800

    Got designer display working

--- Mangosteen/Panels/WheelPanel.cs
+++ Mangosteen/Panels/WheelPanel.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 namespace Mangosteen.Panels
 {
@@ -16,9 +19,37 @@ namespace Mangosteen.Panels
             
         }
 
-        public WheelPanel()
+        private Canvas _designTimeCanvas;
+
+        public WheelPanel() : base()
         {
+            // It is not necessary to remove the event listeners when the object goes away
+            this.Loaded += WheelPanel_Loaded;
             this.SizeChanged += WheelPanel_SizeChanged;
+
+            // Adding children here will cause them not to show up in the designer.
+            // I'm not quite sure why.
+                       
+        }
+
+        void WheelPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                CreateDesignTimeCanvas();
+            }
+        }
+
+        private void CreateDesignTimeCanvas()
+        {
+            // Add the design time canvas
+            _designTimeCanvas = new Canvas();
+            this.Children.Add(_designTimeCanvas);
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = new SolidColorBrush(Color.FromArgb(255, 25, 162, 222));
+            ellipse.Width = 100;
+            ellipse.Height = 100;
+            _designTimeCanvas.Children.Add(ellipse);
         }
 
         void WheelPanel_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -26,10 +57,11 @@ namespace Mangosteen.Panels
             // Use actual width and height to determine the radius.
             double smallestDimension = Math.Min(e.NewSize.Width, e.NewSize.Height);
             SetValue(RadiusProperty, .5 * smallestDimension);
+
+
         }
+
         // Properties Width and Height already inherited
-
-
         public double Radius
         {
             get { return (double)GetValue(RadiusProperty); }
@@ -44,14 +76,15 @@ namespace Mangosteen.Panels
         {
             Size s = base.MeasureOverride(availableSize);
 
-            //foreach (UIElement element in this.Children)
-            //    element.Measure(availableSize);
+            foreach (UIElement element in this.Children)
+                element.Measure(availableSize);
 
             return s;
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
+
             //// Clip to ensure items dont override container
             //this.Clip = new RectangleGeometry { Rect = new Rect(0, 0, finalSize.Width, finalSize.Height) };
 
