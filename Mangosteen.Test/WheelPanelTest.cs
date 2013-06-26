c03e752c1b136f000113c86f25518cfefa157995
Jeremy White <sail.madeline@gmail.com>
2013-06-26 13:04:19 +0800

    First unit test are in place.

--- Mangosteen.Test/WheelPanelTest.cs
+++ Mangosteen.Test/WheelPanelTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mangosteen.Panels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,33 @@ namespace Mangosteen.Test
 {
     public class WheelPanelTest
     {
-        [Fact]
-        public void AssertIsTrue()
+        WheelPanel _unitPanel;
+
+        public WheelPanelTest()
         {
-            Assert.True(true);
+            _unitPanel = new WheelPanel();
+        }
+
+        [Fact]
+        public void WheelPanel_Can_Be_Constructed()
+        {
+            Assert.True(_unitPanel != null);
+        }
+
+        [Fact]
+        public void WidthHeightSetsRadius()
+        {
+            _unitPanel.Width = 100;
+            _unitPanel.Height = 100;
+
+            Assert.True(_unitPanel.ActualRadius == 50);
+        }
+
+        [Fact]
+        public void WidthHeightSetsCenter()
+        {
+            Assert.True(_unitPanel.Center.X == 50 &&
+                        _unitPanel.Center.Y == 50);
         }
     }
 }
