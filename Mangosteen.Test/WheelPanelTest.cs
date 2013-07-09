5352140f818d27536eea3445abc00e2ed1788238
Jeremy White <sail.madeline@gmail.com>
2013-07-09 10:47:27 +0800

    Added more test cases for radius parameters.

--- Mangosteen.Test/WheelPanelTest.cs
+++ Mangosteen.Test/WheelPanelTest.cs
@@ -14,10 +14,18 @@ namespace Mangosteen.Test
 {
     public class WheelPanelTestable : WheelPanel
     {
+        private double radius;
+
         public WheelPanelTestable(double width, double height) : base()
         {
             Width = width;
             Height = height;
+        }
+
+        public WheelPanelTestable(double radius)
+        {
+            // TODO: Complete member initialization
+            this.radius = radius;
         }
     }
 
@@ -37,20 +45,72 @@ namespace Mangosteen.Test
             Assert.True(_unitPanel != null);
         }
 
-        [Fact]
-        public async Task WidthHeightSetsRadius()
+        [Theory]
+        [InlineData(100, 100, 50)]
+        [InlineData(200, 200, 100)]
+        [InlineData(100, 50, 25)]
+        [InlineData(50, 100, 25)]
+        //
+        // Width and height should set the radius unless an outerradius is defined
+        //
+        public async Task WidthHeightSetsRadius(double width, double height, double value)
         {
             await ExecuteOnUIThread<ArgumentException>(() =>
             {
-                _unitPanel = new WheelPanelTestable(100,100);
-                Assert.True(_unitPanel.ActualRadius == 50);
+                _unitPanel = new WheelPanelTestable(width, height);
+                Assert.True(_unitPanel.ActualRadius == value);
             }); 
         }
 
-        [Theory, InlineData(100,100)]
-        public void TestTheory(int width, int height)
+        [Theory]
+        [InlineData(100, 100, 50, 50)]
+        [InlineData(200, 200, 100, 100)]
+        [InlineData(100, 50, 50, 25)]
+        [InlineData(50, 100, 25, 50)]
+        //
+        // Center should always be at the center of the width and height box.
+        //
+        public async Task WidthHeightSetsCenter(double width, double height, double centerx, double centery)
         {
-            Assert.True(width == height);
+            await ExecuteOnUIThread<ArgumentException>(() =>
+            {
+                _unitPanel = new WheelPanelTestable(width, height);
+                Assert.True((_unitPanel.Center.X == centerx) && (_unitPanel.Center.Y == centery));
+            });
+        }
+
+        [Theory]
+        [InlineData(50, 50, 50, 50)]
+        [InlineData(20, 50, 50, 20)]
+        //
+        // The read only property of ActualRadius should be set by the radius (if set) regardless of the 
+        // Width and height.
+        //
+        public async Task RadiusSetsActualRadius(double radius, double width, double height, double value)
+        {
+            await ExecuteOnUIThread<ArgumentException>(() =>
+            {
+                _unitPanel = new WheelPanelTestable(width, height);
+                _unitPanel.OuterRadius = radius;
+                Assert.True(_unitPanel.ActualRadius == value);
+            });
+        }
+
+        [Theory]
+        [InlineData(25, 25)]
+        [InlineData(75, 50)]
+        //
+        // The inner raduis can be 0, but should be bounded by the outer radius.  (It can be no bigger.)
+        //
+        public async Task InnerRadiusIsBoundedByOuterRadius(double innerradius, double value)
+        {
+            await ExecuteOnUIThread<ArgumentException>(() =>
+            {
+                _unitPanel = new WheelPanelTestable(100, 100);
+                _unitPanel.OuterRadius = 50;
+                _unitPanel.InnerRadius = innerradius;
+                Assert.True(_unitPanel.InnerRadius == value);
+            });
         }
     }
 }
