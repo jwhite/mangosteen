b9ab42025a055e5a372bfe8e25704b3e59e21d6b
Jeremy White <sail.madeline@gmail.com>
2013-07-11 15:56:07 +0800

    Now with the metadata table.

---
+++ Mangosteen.Design/metadata.cs
@@ -0,0 +1,36 @@
+﻿
+extern alias ControlLibrary;
+using Win8ControlLibrary = Mangosteen;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Windows.Design.Metadata;
+using Microsoft.Windows.Design.Features;
+using System.ComponentModel;
+
+[assembly: ProvideMetadata(typeof(Mangosteen.Design.Metadata))]
+namespace Mangosteen.Design
+{
+    public class Metadata : IProvideAttributeTable
+    {
+        public AttributeTable AttributeTable
+        {
+            get
+            {
+                AttributeTableBuilder builder = new AttributeTableBuilder();
+
+                // 
+                // Custom attributes should be added to the attribute table here.
+                //
+                //builder.AddCustomAttributes( typeof(Win8ControlLibrary.RedButton), "CustomCount", new CategoryAttribute("CustomCategory"));
+                //builder.AddCustomAttributes( typeof(Win8ControlLibrary.RedButton), "CustomColor", new CategoryAttribute("CustomCategory"));
+                //builder.AddCustomAttributes( typeof(Win8ControlLibrary.RedButton), "CustomBrush", new CategoryAttribute("CustomCategory"));
+              
+                return builder.CreateTable();
+            }
+        }
+    }
+}
