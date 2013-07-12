ecf22798d9906ee939e46d816833af6e60f864b6
Jeremy White <sail.madeline@gmail.com>
2013-07-12 11:59:30 +0800

    New mangosteen extensions pathing for libs directory.

--- Mangosteen.Design/CopyToLibs.bat
+++ Mangosteen.Design/CopyToLibs.bat
@@ -1,11 +1,19 @@
+@echo ON
 :%1 = ProjectDir
 :%2 = Outdir
 echo ProjectDir = %1
 echo OutDir = %2
 
-copy "%~1SDKManifest.xml" "%~1libs\MangosteenSDK\1.0\"
-copy "%~2Mangosteen.pri" "%~1libs\MangosteenSDK\1.0\Redist\CommonConfiguration\neutral\"
+set EXTENSIONDIR=%~1libs\Windows\v8.0\ExtensionSDKs\
 
-copy "%~2themes\generic.xaml" "%~1libs\MangosteenSDK\1.0\Redist\CommonConfiguration\neutral\themes\"
-copy "%~2Mangosteen.dll" "%~1libs\MangosteenSDK\1.0\References\CommonConfiguration\neutral\"
-copy "%~2Mangosteen.Design.dll" "%~1libs\MangosteenSDK\1.0\DesignTime\CommonConfiguration\neutral\"
+echo ExtensionDir = %EXTENSIONDIR%
+set CONTROLSDIR=%EXTENSIONDIR%MangosteenSDK\1.0\
+
+echo ControlsDir = %CONTROLSDIR%
+
+copy "%~1SDKManifest.xml" "%CONTROLSDIR%"
+copy "%~2Mangosteen.pri" "%CONTROLSDIR%\Redist\CommonConfiguration\neutral\"
+
+copy "%~2themes\generic.xaml" "%CONTROLSDIR%\Redist\CommonConfiguration\neutral\themes\"
+copy "%~2Mangosteen.dll" "%CONTROLSDIR%\References\CommonConfiguration\neutral\"
+copy "%~2Mangosteen.Design.dll" "%CONTROLSDIR%\DesignTime\CommonConfiguration\neutral\"
