e02452f14e8e55ad1ed59f02ca17a3a7db964734
Jeremy White <sail.madeline@gmail.com>
2013-07-27 09:32:17 +0800

    Looking at ui tests

---
+++ Mangosteen.Test/WheelPanelTest_UI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Mangosteen.Test
+{
+    public class WheelPanelTest_UI
+    {
+        [Fact]
+        public void WheelPanel_Can_Be_Hosted()
+        {
+            Assert.True(false);
+        }
+    }
+}
