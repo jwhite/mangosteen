717543abe63cf3e937cd01410a3ae6e8a44c6206
Jeremy White <sail.madeline@gmail.com>
2013-08-06 12:30:48 +0800

    Added new calculation class.

---
+++ Mangosteen/Panels/Wedge/WedgeAngleCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Mangosteen.Panels.Wedge
+{
+    public class WedgeAngleCalculation
+    {
+        public WedgeAngleCalculation(WedgeAngle wa, FrameworkElement e)
+        {
+            WedgeAngle = wa;
+            Element = e;
+        }
+
+        public double CalculatedWedgeAngle;
+
+        public WedgeAngle WedgeAngle;
+        public FrameworkElement Element;
+    }
+}
