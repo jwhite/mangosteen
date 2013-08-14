67b67040e851fc9d29d8540bbb839d14f77f23f8
Jeremy White <sail.madeline@gmail.com>
2013-08-14 17:30:43 +0800

    More messing around with async

---
+++ Mangosteen.Test/AsyncHelpers/AsyncOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mangosteen.Test
+{
+    // Our single operation the the queue will keep track of, 
+    // then call invoke when it is appropriate.
+    internal class AsyncOperation
+    {
+        private readonly SendOrPostCallback _action;
+        private readonly object _state;
+
+        public AsyncOperation(SendOrPostCallback action, object state)
+        {
+            _action = action;
+            _state = state;
+        }
+        public void Invoke()
+        {
+            _action(_state);
+        }
+    }
+}
--- Mangosteen.Test/AsyncHelpers/AsyncOperationsQueue.cs
+++ Mangosteen.Test/AsyncHelpers/AsyncOperationsQueue.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
 
 namespace Mangosteen.Test
 {
-    internal class AsyncOperationsQueue
+    internal class AsyncOperationsQueue<T>  where T : AsyncOperation
     {
+        private bool _run = true;
+        private readonly Queue<T>  _operations = new Queue<T>();
+        private readonly AutoResetEvent _operationsAvailable = new AutoResetEvent(false);
+
+        public void Enque(T asyncOperation)
+        {
+            _operations.Enqueue(asyncOperation);
+            _operationsAvailable.Set();
+        }
+
+        // NOTE : not quite sure what this is doing.
+        public void MarkAsComplete()
+        {
+            _run = false;
+            _operationsAvailable.Set();
+        }
+
+        private void InvokePendingOperations()
+        {
+            while ( _operations.Count > 0)
+            {
+                T operation = _operations.Dequeue();
+                operation.Invoke();
+            }
+        }
+
+        public void InvokeAll()
+        {
+            while (_run)
+            {
+                InvokePendingOperations();
+                _operationsAvailable.WaitOne();
+            }
+
+            InvokePendingOperations();
+        }
     }
 }
