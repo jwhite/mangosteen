1477d0149a039418b1c65c662b6137d25eeb723a
Jeremy White <sail.madeline@gmail.com>
2013-08-15 10:48:58 +0800

    Sync context done

--- Mangosteen.Test/AsyncHelpers/AsyncSynchronizationContext.cs
+++ Mangosteen.Test/AsyncHelpers/AsyncSynchronizationContext.cs
@@ -9,6 +9,35 @@ namespace Mangosteen.Test
 {
     class AsyncSynchronizationContext : SynchronizationContext
     {
+        private int _operationCount;
+        private readonly AsyncOperationsQueue<AsyncOperation> _operations = new AsyncOperationsQueue<AsyncOperation>();
 
+        // Asynchronous call the the delegate
+        // 
+        // Send occurs like normal (synchronous) but posts get queue up
+        // This should handle the problem with void Tasks?
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _operations.Enque(new AsyncOperation(d, state));
+        }
+
+        public override void OperationCompleted()
+        {
+            if (Interlocked.Decrement(ref _operationCount) == 0)
+            {
+                base.OperationCompleted();
+            }
+        }
+
+        public override void OperationStarted()
+        {
+            Interlocked.Increment(ref _operationCount);
+            base.OperationStarted();
+        }
+
+        public void WaitForPendingOperationsToComplete()
+        {
+            _operations.InvokeAll();
+        }
     }
 }
