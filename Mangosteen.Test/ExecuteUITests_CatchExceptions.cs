1fe89940cdd90947460677594b39620a66f84eba
Jeremy White <sail.madeline@gmail.com>
2013-08-29 20:33:41 +0800

    Exception throwing now testable.

--- Mangosteen.Test/ExecuteUITests_CatchExceptions.cs
+++ Mangosteen.Test/ExecuteUITests_CatchExceptions.cs
@@ -8,62 +8,77 @@ using Xunit;
 
 namespace Mangosteen.Test.Images
 {
-    class ExecuteUITest_CatchExceptions
+    public class ExecuteUITest_CatchExceptions
     {
+        public class ThrowTestException : Exception
+        {
+            public ThrowTestException(string message) : base(message) { } 
+        }
+
         private string _shouldBeSet;
 
         [Fact]
         public void TryToCall_TakesNoneReturnsVoid()
         {
-            ExecuteUIThread.ExecuteOnUIThread(() =>
+            Assert.Throws<ThrowTestException>(() =>
             {
-                Button b = new Button();
-                _shouldBeSet = "How now brown cow";
-            });
+                ExecuteUIThread.ExecuteOnUIThread(() =>
+                {
+                    Button b = new Button();
+                    _shouldBeSet = "How now brown cow";
 
-            Assert.True(_shouldBeSet == "How now brown cow");
+                    throw new ThrowTestException("message for the user");
+                });
+           });
         }
 
         [Fact]
         public void TryToCall_TakesOneReturnsVoid()
         {
-            int i = 7;
-            ExecuteUIThread.ExecuteOnUIThread(() =>
+            Assert.Throws<ThrowTestException>(() =>
             {
-                Button b = new Button();
-                _shouldBeSet = "How now brown cow " + i;
-            });
+                int i = 7;
+                ExecuteUIThread.ExecuteOnUIThread(() =>
+                {
+                    Button b = new Button();
+                    _shouldBeSet = "How now brown cow " + i;
 
-            Assert.True(_shouldBeSet == "How now brown cow 7");
+                    throw new ThrowTestException("message for the user");
+                });
+            });
         }
 
         [Fact]
         public void TryToCall_TakesNoneReturnsObject()
         {
-            object retval = ExecuteUIThread.ExecuteOnUIThread(() =>
+            Assert.Throws<ThrowTestException>(() =>
             {
-                Button b = new Button();
-                _shouldBeSet = "How now brown cow";
-                return (int)11;
-            });
+                object retval = ExecuteUIThread.ExecuteOnUIThread(() =>
+                {
+                    Button b = new Button();
+                    _shouldBeSet = "How now brown cow";
+                    return (int)11;
 
-            Assert.True((int)retval == 11);
-            Assert.True(_shouldBeSet == "How now brown cow");
+                    throw new ThrowTestException("message for the user");
+                });
+            });
         }
 
         [Fact]
         public void TryToCall_TakesOneReturnsObject()
         {
-            int i = 22;
-            object retval = ExecuteUIThread.ExecuteOnUIThread(() =>
+            Assert.Throws<ThrowTestException>(() =>
             {
-                Button b = new Button();
-                _shouldBeSet = "How now brown cow " + i;
-                return (int)22;
-            });
+                int i = 22;
+                object retval = ExecuteUIThread.ExecuteOnUIThread(() =>
+                {
+                    Button b = new Button();
+                    _shouldBeSet = "How now brown cow " + i;
+                    return (int)22;
 
-            Assert.True((int)retval == 22);
-            Assert.True(_shouldBeSet == "How now brown cow 22");
+                    throw new ThrowTestException("message for the user");
+                });
+            });
         }
     }
 }
